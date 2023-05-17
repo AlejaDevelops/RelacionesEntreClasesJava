6a3d9b84b4dbefa777cf3b4685aa877d61f4a507
Alejandra Orjuela <orjuela.alejandrap@gmail.com>
2023-05-17 15:56:10 -0500

    1st commit

---
+++ src/PracticasIniciales/PersonaMain.java
@@ -0,0 +1,22 @@
+/*
+EJERCICIO PERSONA
+Realiza un programa en donde una clase Persona tenga como atributo nombre, apellido y un
+objeto de clase Dni. La clase Dni tendrá como atributos la serie (carácter) y número. Prueba
+acceder luego a los atributos del dni de la persona creando objetos y jugando desde el main.
+ */
+package PracticasIniciales;
+
+/**
+ *
+ * @author AlejaDevelops
+ */
+public class PersonaMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
